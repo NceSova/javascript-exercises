ebcd31ecf6c975922f62f671c6c3cc2268d02f2b
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-03 18:21:34 +0300

    added 07 code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  convertedTemp = ((tempInFahrenheit-32) * 5/9).toFixed(1);
+  if(Number.isInteger(convertedTemp)) {
+    return Number.parseInt(convertedTemp.toFixed(0));
+  } else {
+    return Number.parseFloat(convertedTemp);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  convertedTemp = (tempInCelsius * 9/5+32).toFixed(1);
+  if(Number.isInteger(convertedTemp)) {
+    return Number.parseInt(convertedTemp.toFixed(0));
+  } else {
+    return Number.parseFloat(convertedTemp);
+  }
 };
 
 // Do not edit below this line
