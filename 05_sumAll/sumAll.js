fdaa66eea1bc4384e8fffb2e4be575c7ef375102
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-03 17:42:32 +0300

    add 05 code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(fromNum, toNum) {
+  let sum = 0;
+  if (typeof fromNum !== 'number' || typeof toNum !== 'number') {
+    return 'ERROR'
+  } else if (fromNum < 0 || toNum < 0) {
+    return 'ERROR'
+  }
+  if (fromNum < toNum) {
+    for (let i = fromNum; i <= toNum; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = fromNum; i >= toNum; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 
 };
 
